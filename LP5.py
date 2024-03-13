bde09f82d486dd6edac89d8b184dc2fa0262bc7e
faithenstein <bda20014787@gmail.com>
2024-03-13 10:09:33 +0300

    Добавьте файлы проекта.

---
+++ LP5.py
@@ -0,0 +1,21 @@
+﻿# Лабораторная работа №5
+# Выполняли Бахтин Данила, Бритвин Антон
+# ДКИП-205
+# Вариант 3-4
+
+def hex_sum_table():
+    print("Hex Addition Table:")
+    for i in range(16):
+        for j in range(16):
+            print(f"{hex(i)} + {hex(j)} = {hex(i + j)}")
+        print("")
+
+def hex_mult_table():
+    print("Hex Multiplication Table:")
+    for i in range(16):
+        for j in range(16):
+            print(f"{hex(i)} * {hex(j)} = {hex(i * j)}")
+        print("")
+
+hex_sum_table()
+hex_mult_table()
